8637d23927e6664cc02c40c5b2caa1baed4023b9
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-06-15 22:55:47 -0400

    introducing paris

---
+++ src/components/carouselParis/Images.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import '../../stylesheets/AboutParis.css'
+
+const Images = [
+  {
+    img: (
+      <img
+        src='https://imgur.com/Goh5CJu.png'
+        className='images-spread paris-opener'
+        alt='paris cover'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/AG47vZv.jpg'
+        className='images-spread'
+        alt='park spread'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/GeTVIXU.png'
+        className='images-spread'
+        alt='other park spread'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/58CEeOw.png'
+        className='images-spread'
+        alt='chef at home'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/nnorFVz.png'
+        className='images-spread'
+        alt='waiter'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/LTNOm3M.png'
+        className='images-spread'
+        alt='paris and food'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/CSkcLup.png'
+        className='images-spread'
+        alt='market'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/iTXVcpf.png'
+        className='images-spread'
+        alt='girl at cafe'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/UswFLRh.png'
+        className='images-spread'
+        alt='girl chefs'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/DrRCHsx.png'
+        className='images-spread'
+        alt='flowers spread'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/U0aEoTl.png'
+        className='images-spread'
+        alt='young chef'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/gyoRUev.png'
+        className='images-spread'
+        alt='radishes'></img>
+    ),
+  },
+  {
+    img: (
+      <img
+        src='https://imgur.com/jJaI89t.png'
+        className='images-spread'
+        alt='oysters'></img>
+    ),
+  },
+]
+export default Images
---
+++ src/components/carouselParis/Slide.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import imageData from '../carouselMexico/Images'
+import '../../stylesheets/AboutMexico.css'
+
+class Slide extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { landing: imageData }
+  }
+  render() {
+    return (
+      <section>
+        {this.state.landing.map((s, index) => (
+          <div
+            className={
+              index === this.props.activeIndex
+                ? 'slide-active'
+                : 'slide-sliding'
+            }
+            key={index}>
+            {s.img}
+          </div>
+        ))}
+      </section>
+    )
+  }
+}
+export default Slide
