80375ea23f0a14858b2391db467acf4fdfccfbbc
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-06-18 16:01:51 -0400

    changed mexico navs

--- src/components/mexico/AboutMexico.js
+++ src/components/mexico/AboutMexico.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import imageData from './MexicoImages'
+import NavbarMexico from '../../navbars/navbarMexico/NavbarMexico'
+import NavbarMexicoMobileInt from '../../navbars/navbarMexico/NavbarMexicoMobileInt'
 import AboutWF from '../aboutWF/AboutWF'
 
 const AboutMexico = () => {
-  return <AboutWF imageData={imageData} />
+  return (
+    <AboutWF
+      imageData={imageData}
+      navbar={<NavbarMexico />}
+      navbarMobileInt={<NavbarMexicoMobileInt />}
+    />
+  )
 }
 
 export default AboutMexico
--- src/components/mexico/AboutMexicoMobile.js
+++ src/components/mexico/AboutMexicoMobile.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import AboutWFMobile from '../aboutWF/AboutWFMobile'
 import MexicoMobileImages from './MexicoMobileImages'
+import NavbarMexico from '../../navbars/navbarMexico/NavbarMexico'
+import NavbarMexicoMobile from '../../navbars/navbarMexico/NavbarMexicoMobile'
 
 const AboutMexicoMobile = () => {
-  return <AboutWFMobile mobileImages={<MexicoMobileImages />} />
+  return (
+    <AboutWFMobile
+      mobileImages={<MexicoMobileImages />}
+      navbar={<NavbarMexico />}
+      navbarMobile={<NavbarMexicoMobile />}
+    />
+  )
 }
 
 export default AboutMexicoMobile
